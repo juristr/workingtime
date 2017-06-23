a8ee71474cb8ce363813f6adeb6962bdad89bf53
Juri <juri.strumpflohner@gmail.com>
2017-06-23 23:47:24 +0200

    feat(parser): add timestamp parsing

--- src/app/timeeditor/parser.service.spec.ts
+++ src/app/timeeditor/parser.service.spec.ts
@@ -3,6 +3,8 @@ import { TestBed, inject } from '@angular/core/testing';
 import { ITimeStamp } from '../core';
 import { ParserService } from './parser.service';
 
+import * as moment from 'moment';
+
 describe('ParserService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -15,11 +17,35 @@ describe('ParserService', () => {
   }));
 
   it('should correctly parse the input time into slices', inject([ParserService], (service: ParserService) => {
-    let inputTime = `E07:45 U12:48
+    const inputTime = `E07:45 U12:48
+E13:38 U16:45`;
+    const resultTimes = service.parseAttentanceTimestamps(inputTime);
+    expect(resultTimes.length).toBe(4);
+
+    expect(moment(resultTimes[0]).format('HH:mm')).toEqual('07:45');
+    expect(moment(resultTimes[1]).format('HH:mm')).toEqual('12:48');
+    expect(moment(resultTimes[2]).format('HH:mm')).toEqual('13:38');
+    expect(moment(resultTimes[3]).format('HH:mm')).toEqual('16:45');
+  }));
+
+    it('should correctly parse the input time when last exit time is missing', inject([ParserService], (service: ParserService) => {
+    const inputTime = `E07:45 U12:48
 E13:38`;
-    const resultTimes: ITimeStamp = service.parseAttentanceTimestamps(inputTime);
+    const resultTimes = service.parseAttentanceTimestamps(inputTime);
+    expect(resultTimes.length).toBe(3);
 
+    expect(moment(resultTimes[0]).format('HH:mm')).toEqual('07:45');
+    expect(moment(resultTimes[1]).format('HH:mm')).toEqual('12:48');
+    expect(moment(resultTimes[2]).format('HH:mm')).toEqual('13:38');
+  }));
 
-  });
+      it('should also only parse two timestamps (like just in the morning)', inject([ParserService], (service: ParserService) => {
+    const inputTime = `E07:45 U12:48`;
+    const resultTimes = service.parseAttentanceTimestamps(inputTime);
+    expect(resultTimes.length).toBe(2);
+
+    expect(moment(resultTimes[0]).format('HH:mm')).toEqual('07:45');
+    expect(moment(resultTimes[1]).format('HH:mm')).toEqual('12:48');
+  }));
 
 });
--- src/app/timeeditor/parser.service.ts
+++ src/app/timeeditor/parser.service.ts
@@ -1,14 +1,28 @@
 import { Injectable } from '@angular/core';
 
 import { ITimeStamp } from '../core';
+import * as moment from 'moment';
 
 @Injectable()
 export class ParserService {
+  // private _regex = /\[E(\d{2}\:\d{2})\]*\[\sU(\d{2}\:\d{2})\]*/g;
+  private _regex = /[[E|U](\d{2}\:\d{2})]*/g;
 
   constructor() { }
 
-  parseAttentanceTimestamps(inputTimes: string): ITimeStamp {
-    return null;
-  }
+  parseAttentanceTimestamps(inputTimes: string): Date[] {
+    const matchedTimes = [];
 
+    let match = this._regex.exec(inputTimes);
+
+    while (match != null) {
+      for (let i = 1; i < match.length; i++) {
+        matchedTimes.push(match[i]);
+      }
+
+      match = this._regex.exec(inputTimes);
+    }
+
+    return matchedTimes.map(x => moment(x, 'HH:mm').toDate());
+  }
 }
