d457907c6020a1dba3972e67dc7b2023f18104fd
Juri <juri.strumpflohner@gmail.com>
2017-06-25 15:30:23 +0200

    feat(timecalculator): add calculations of full days

--- src/app/core/timecalculator.service.spec.ts
+++ src/app/core/timecalculator.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed, inject } from '@angular/core/testing';
+import * as moment from 'moment';
 
 import { TimecalculatorService } from './timecalculator.service';
 
@@ -9,7 +10,39 @@ describe('TimecalculatorService', () => {
     });
   });
 
-  it('should be created', inject([TimecalculatorService], (service: TimecalculatorService) => {
-    expect(service).toBeTruthy();
-  }));
+  it(
+    'should be created',
+    inject([TimecalculatorService], (service: TimecalculatorService) => {
+      expect(service).toBeTruthy();
+    })
+  );
+
+  it(
+    'should correctly calculate hours for a complete day',
+    inject([TimecalculatorService], (service: TimecalculatorService) => {
+      const stamps = [
+        moment('08:00', 'HH:mm').toDate(),
+        moment('12:00', 'HH:mm').toDate(),
+        moment('14:00', 'HH:mm').toDate(),
+        moment('18:00', 'HH:mm').toDate()
+      ];
+
+      const workingStats = service.calculateWorkingHours(stamps);
+      expect(moment(workingStats.hoursWorked).utc().hours()).toBe(8);
+    })
+  );
+
+  it(
+    'should correctly calculate hours for a missing stamp at the end of the day',
+    inject([TimecalculatorService], (service: TimecalculatorService) => {
+      const stamps = [
+        moment('08:00', 'HH:mm').toDate(),
+        moment('12:00', 'HH:mm').toDate(),
+        moment('14:00', 'HH:mm').toDate()
+      ];
+
+      const workingStats = service.calculateWorkingHours(stamps);
+      expect(moment(workingStats.hoursWorked).utc().hours()).toBe(8);
+    })
+  );
 });
--- src/app/core/timecalculator.service.ts
+++ src/app/core/timecalculator.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import * as moment from 'moment';
 
 @Injectable()
 export class TimecalculatorService {
+  constructor() {}
 
-  constructor() { }
+  calculateWorkingHours(stamps: Date[]) {
+    let hoursWorked = 0;
 
+    for (let i = 0; i < stamps.length - 1; i++) {
+      hoursWorked += moment(stamps[i + 1]).diff(moment(stamps[i]));
+      i++;
+    }
+
+    return {
+      hoursWorked: hoursWorked
+    };
+  }
 }
